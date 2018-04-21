b00d8112734d28615be1c6e9f2845537615a5b03
eveleen <2637258906@qq.com>
2018-04-22 00:18:40 +0800

    fe almost done

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import {
     App, LoginIndex, Login, Register, ProductIndex, HomePage, DetailInfo, Remark,
-    OrderItem, MenuIndex, CartPage, Account, Collect, BuyItem
+    OrderItem, MenuIndex, CartPage, Account, Collect, BuyItem, AddressList, AccountMenu,
+    EditAddress, ProductList, EditProduct
 } from '../components';
 import { BASE_URL } from '../../conf/config';
 import { adminOperationTypeEnum } from '../data/enum';
-import Success from '../components/common/Success/success'
+import Success from '../components/common/Success/success';
 
 const baseUrl = BASE_URL.substring(1);
 
@@ -20,13 +21,27 @@ const routes = (
                 <Route path={"/home"} component={HomePage} />
                 <Route path={"/buy"} component={BuyItem} />
                 <Route path={"/item"} component={DetailInfo} />
-                <Route path={"/account"} component={Account} />
+                {/* <Route path={"/account"} component={Account} /> */}
+                {/* <Route path={"/address"} component={AddressList} /> */}
                 <Route path={"/collectTreasure"} component={Collect} />
                 <Route path={"/collectShop"} component={Collect} />
                 <Route path={"/order"} component={OrderItem} />
                 <Route path={"/cart"} component={CartPage} />
                 <Route path={"/remark"} component={Remark} />
                 <Route path={"/success"} component={Success} />
+
+                <Route path={"/account"} component={App}>
+                    <IndexRoute component={Account} />
+                    <Route path="member" component={Account} />
+                    <Route path="address" component={AddressList} />
+                    <Route path="editAddress" component={EditAddress} />
+                </Route>
+
+                <Route path={"/openShop"} component={App}>
+                    <IndexRoute component={ProductList} />
+                    <Route path="productList" component={ProductList} />
+                    <Route path="editProduct" component={EditProduct} />
+                </Route>
             </Route>
         </Route>
     </Router >
