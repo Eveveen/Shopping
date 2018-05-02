2e5cc69f1dd0409d2fd83908560693d2426c3a05
esun <Eveleen.sun@gmail.com>
2018-05-02 16:37:25 +0800

    add super

---
+++ server/controllers/admin.js
@@ -0,0 +1,212 @@
+const router = require("express").Router();
+const path = require('path');
+const config = require('../../conf/config');
+const utils = require('../common/utils');
+const httpAgent = require('../common/httpAgent');
+const log4js = require('log4js');
+const logger = log4js.getLogger();
+const fs = require('fs');
+const multer = require('multer')
+const paramsMulter = multer({ dest: 'upload/' });
+
+/**
+ * 获取所有的店铺
+ */
+router.get("/getAllShop", function (req, res) {
+    const path = utils.PROJECT + "/getAllShop";
+    httpAgent.httpRequest({}, "json", config.BACKEND_API.TYPE, config.BACKEND_API.HOST, config.BACKEND_API.PORT, path, "get", function (data) {
+        res.send(data);
+    }, function (statusCode, msg) {
+        res.send({ error: { code: -1, msg: msg } });
+    })
+});
+
+/**
+ * 获取所有的卖家
+ */
+router.get("/getAllSeller", function (req, res) {
+    const path = utils.PROJECT + "/getAllSeller";
+    httpAgent.httpRequest({}, "json", config.BACKEND_API.TYPE, config.BACKEND_API.HOST, config.BACKEND_API.PORT, path, "get", function (data) {
+        res.send(data);
+    }, function (statusCode, msg) {
+        res.send({ error: { code: -1, msg: msg } });
+    })
+});
+
+/**
+ * 根据卖家编号获取卖家信息
+ */
+router.get("/getSeller/:id", function (req, res) {
+    const path = utils.PROJECT + "/getSeller";
+    httpAgent.httpRequest({ sellerId: req.params.id }, "json", config.BACKEND_API.TYPE, config.BACKEND_API.HOST, config.BACKEND_API.PORT, path, "get", function (data) {
+        res.send(data);
+    }, function (statusCode, msg) {
+        res.send({ error: { code: -1, msg: msg } });
+    })
+});
+
+/**
+ * 添加卖家
+ */
+router.post("/addSeller", function (req, res) {
+    const path = utils.PROJECT + "/addSeller";
+    const { data } = req.body;
+    httpAgent.httpRequest(data, "json", config.BACKEND_API.TYPE, config.BACKEND_API.HOST, config.BACKEND_API.PORT, path, "get", function (data) {
+        res.send(data);
+    }, function (statusCode, msg) {
+        res.send({ error: { code: -1, msg: msg } });
+    })
+});
+
+
+/**
+ * 编辑卖家信息
+ */
+router.post("/editSeller", function (req, res) {
+    const path = utils.PROJECT + "/editSeller";
+    const { data } = req.body;
+    httpAgent.httpRequest(data, "json", config.BACKEND_API.TYPE, config.BACKEND_API.HOST, config.BACKEND_API.PORT, path, "get", function (data) {
+        res.send(data);
+    }, function (statusCode, msg) {
+        res.send({ error: { code: -1, msg: msg } });
+    })
+});
+
+/**
+ * 删除卖家，应该级联删除店铺
+ */
+router.get("/deleteSeller/:id", function (req, res) {
+    const path = utils.PROJECT + "/deleteSeller";
+    httpAgent.httpRequest({ sellerId: req.params.id }, "json", config.BACKEND_API.TYPE, config.BACKEND_API.HOST, config.BACKEND_API.PORT, path, "get", function (data) {
+        res.send(data);
+    }, function (statusCode, msg) {
+        res.send({ error: { code: -1, msg: msg } });
+    })
+});
+
+/**
+ * 获取卖家的店铺
+ */
+router.get("/getSellerShop", function (req, res) {
+    const path = utils.PROJECT + "/getSellerShop";
+    httpAgent.httpRequest({}, "json", config.BACKEND_API.TYPE, config.BACKEND_API.HOST, config.BACKEND_API.PORT, path, "get", function (data) {
+        res.send(data);
+    }, function (statusCode, msg) {
+        res.send({ error: { code: -1, msg: msg } });
+    })
+});
+
+/**
+ * 添加店铺，一个卖家只能有一个店铺
+ */
+router.post("/addShop", function (req, res) {
+    const path = utils.PROJECT + "/addShop";
+    const { data } = req.body;
+    httpAgent.httpRequest(data, "json", config.BACKEND_API.TYPE, config.BACKEND_API.HOST, config.BACKEND_API.PORT, path, "get", function (data) {
+        res.send(data);
+    }, function (statusCode, msg) {
+        res.send({ error: { code: -1, msg: msg } });
+    })
+});
+
+/**
+ * 编辑店铺
+ */
+router.post("/editShop", function (req, res) {
+    const { data } = req.body;
+    const path = utils.PROJECT + "/editShop";
+    httpAgent.httpRequest(data, "json", config.BACKEND_API.TYPE, config.BACKEND_API.HOST, config.BACKEND_API.PORT, path, "get", function (data) {
+        res.send(data);
+    }, function (statusCode, msg) {
+        res.send({ error: { code: -1, msg: msg } });
+    })
+});
+
+/**
+ * 删除店铺
+ */
+router.get("/deleteShop/:id", function (req, res) {
+    const path = utils.PROJECT + "/deleteShop";
+    httpAgent.httpRequest({ shopId: req.params.id }, "json", config.BACKEND_API.TYPE, config.BACKEND_API.HOST, config.BACKEND_API.PORT, path, "get", function (data) {
+        res.send(data);
+    }, function (statusCode, msg) {
+        res.send({ error: { code: -1, msg: msg } });
+    })
+});
+
+
+/**
+ * 获取所有的买家
+ */
+router.get("/getAllUser", function (req, res) {
+    const path = utils.PROJECT + "/getAllUser";
+    httpAgent.httpRequest({}, "json", config.BACKEND_API.TYPE, config.BACKEND_API.HOST, config.BACKEND_API.PORT, path, "get", function (data) {
+        res.send(data);
+    }, function (statusCode, msg) {
+        res.send({ error: { code: -1, msg: msg } });
+    })
+});
+
+/**
+ * 根据买家编号获取买家信息
+ */
+router.get("/getUser/:id", function (req, res) {
+    const path = utils.PROJECT + "/getUser";
+    httpAgent.httpRequest({ userId: req.params.id }, "json", config.BACKEND_API.TYPE, config.BACKEND_API.HOST, config.BACKEND_API.PORT, path, "get", function (data) {
+        res.send(data);
+    }, function (statusCode, msg) {
+        res.send({ error: { code: -1, msg: msg } });
+    })
+});
+
+/**
+ * 添加买家
+ */
+router.post("/addUser", function (req, res) {
+    const path = utils.PROJECT + "/addUser";
+    const { data } = req.body;
+    httpAgent.httpRequest(data, "json", config.BACKEND_API.TYPE, config.BACKEND_API.HOST, config.BACKEND_API.PORT, path, "get", function (data) {
+        res.send(data);
+    }, function (statusCode, msg) {
+        res.send({ error: { code: -1, msg: msg } });
+    })
+});
+
+
+/**
+ * 编辑买家信息
+ */
+router.post("/updateUser", function (req, res) {
+    const { data } = req.body;
+    const path = utils.PROJECT + "/updateUser";
+    httpAgent.httpRequest(data, "json", config.BACKEND_API.TYPE, config.BACKEND_API.HOST, config.BACKEND_API.PORT, path, "get", function (data) {
+        res.send(data);
+    }, function (statusCode, msg) {
+        res.send({ error: { code: -1, msg: msg } });
+    })
+});
+
+/**
+ * 删除买家
+ */
+router.get("/deleteUser/:id", function (req, res) {
+    const path = utils.PROJECT + "/deleteUser";
+    httpAgent.httpRequest({ userId: req.params.id }, "json", config.BACKEND_API.TYPE, config.BACKEND_API.HOST, config.BACKEND_API.PORT, path, "get", function (data) {
+        res.send(data);
+    }, function (statusCode, msg) {
+        res.send({ error: { code: -1, msg: msg } });
+    })
+});
+
+function checkIsRole(req) {
+    const roles = req.session.principal && req.session.principal.roles ? req.session.principal.roles : [];
+    for (var i = 0; i < roles.length; i++) {
+        if (roles[i] == 'admin') {
+            break;
+        }
+    }
+    let flag = i < roles.length ? true : false;
+    return flag;
+}
+
+module.exports = router;
